4b07d857d353f67a84a8d0f4d76fc10b71950dcc
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2018-07-23 16:16:17 +0200

    Initial commit for sweep mode test

---
+++ pyIMD/tests/testPyIMD_Sweep.py
@@ -0,0 +1,8 @@
+from pyIMD.inertialmassdetermination import InertialMassDetermination
+
+# @todo: add demo files to this folder
+file_path1 = "C:\\Users\\localadmin\\ownCloud\\Projects\\Collaborations\\David_Gotthold\\20170712_RSN_3_B"
+file_path2 = "C:\\Users\\localadmin\\ownCloud\\Projects\\Collaborations\\David_Gotthold\\20170712_RSN_3_A"
+file_path3 = "C:\\Users\\localadmin\\ownCloud\\Projects\\Collaborations\\David_Gotthold\\20180703_SBC_3_C_2_A_long_term.tdms"
+obj = InertialMassDetermination(file_path1, file_path2, file_path3, '\t', 23, 0)
+obj.run_intertial_mass_determination()
