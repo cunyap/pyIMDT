85cfba01994fd696a1444c8fa64c4cb945a0bdf1
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2018-07-25 13:33:03 +0200

    added file type method

--- pyIMD/file_io/write_to_disk.py
+++ pyIMD/file_io/write_to_disk.py
@@ -13,16 +13,18 @@ def write_to_png(plot_object, file, **kwargs):
     Kwargs:
     :keyword width:             Figure width (optional)
     :keyword height:            Figure height (optional)
+    :keyword units:             Figure units (optional) 'in', 'mm' or 'cm'
     :keyword resolution:        Figure resolution in dots per inch [dpi] (optional)
 
     Returns:
     :return:                   Writes figure to disk
     """
-    if 'width' and 'height' and 'resolution' in kwargs:
+    if 'width' and 'height' and 'units' and 'resolution' in kwargs:
         width = kwargs.get('width')
         height = kwargs.get('height')
+        units = kwargs.get('units')
         resolution = kwargs.get('resolution')
-        plot_object.save(filename='{}.png'.format(file), width=width, height=height, dpi=resolution)
+        plot_object.save(filename='{}.png'.format(file), width=width, height=height, units=units, dpi=resolution)
     elif not kwargs:
         plot_object.save(filename='{}.png'.format(file))
     else:
@@ -39,21 +41,29 @@ def write_to_pdf(plot_object, file, **kwargs):
     Kwargs:
     :keyword width:             Figure width (optional)
     :keyword height:            Figure height (optional)
+    :keyword units:             Figure units (optional) 'in', 'mm' or 'cm'
     :keyword resolution:        Figure resolution in dots per inch [dpi] (optional)
 
     Returns:
     :return:                    Writes figure to disk
 
     """
-    if 'width' and 'height' and 'resolution' in kwargs:
+    if 'width' and 'height' and 'units' and 'resolution' in kwargs:
         width = kwargs.get('width')
         height = kwargs.get('height')
+        units = kwargs.get('units')
         resolution = kwargs.get('resolution')
-        plot_object.save(filename='{}.pdf'.format(file), width=width, height=height, dpi=resolution)
+        plot_object.save(filename='{}.pdf'.format(file), width=width, height=height, units=units, dpi=resolution)
     elif not kwargs:
         plot_object.save(filename='{}.pdf'.format(file))
     else:
         raise ArgumentError(write_to_pdf.__doc__)
 
 
-
+def write_to_disk_as(file_format, plot_object, file, **kwargs):
+    if file_format == 'pdf':
+        write_to_pdf(plot_object, file, **kwargs)
+    elif file_format == 'png':
+        write_to_png(plot_object, file, **kwargs)
+    else:
+        raise Exception("This figure format is currently not supported.")
