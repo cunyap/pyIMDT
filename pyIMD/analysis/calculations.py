3eaacdd2d43172dc38cfcbfbb975f5b9ce938744
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2018-07-29 08:26:11 +0200

    made fitting parameter user definable

--- pyIMD/analysis/calculations.py
+++ pyIMD/analysis/calculations.py
@@ -22,25 +22,29 @@ def calculate_mass(spring_constant, res_freq_after_cell_load_array, res_freq_bef
     return mass
 
 
-def calculate_resonance_frequencies(frequency_array, phase_array):
+def calculate_resonance_frequencies(frequency_array, phase_array, initial_param_guess, lower_param_bounds,
+                                    upper_param_bounds):
     """calculate_resonance_frequencies Calculates the resonance frequency
        from input frequency and phase array.
 
     Args:
     :param frequency_array:          Array of frequencies [in kHz]
     :param phase_array:              Array of phase [in Rad]
+    :param initial_param_guess:      Initial parameter guess (1x4 array)
+    :param lower_param_bounds:       Lower bounds (1x4 array)
+    :param upper_param_bounds:      Upper bounds (1x4 array)
 
     Returns:
     :retrun resonance_frequency:     Resonance frequency [in kHz]
     :retrun params:                  Curve fit parameters;
                                      param[0] := Q factor (losses)
                                      param[1] := Linear factor accounting for a linear background
-                                     param[2] := Offset of the background@TODO name each of them!
+                                     param[2] := Offset of the background
 
     """
 
     params, _ = optimize.curve_fit(fit_function, frequency_array.astype(float), phase_array.astype(float),
-                                   p0=[70.0, 2.0, 0.5, 0], bounds=([10.0, 1.0, -0.5, -1.0], [80.0, 5.0, 1, 1.0]))
+                                   p0=initial_param_guess, bounds=(lower_param_bounds, upper_param_bounds))
 
     resonance_frequency = params[0]
     return resonance_frequency, params[1:]
