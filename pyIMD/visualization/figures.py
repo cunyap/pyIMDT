d7bf5a6c8e622601c8632f8a7b46198b71dc3400
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2018-08-07 09:16:52 +0200

    added support for plotting the rolling mean directly from a pd.DataFrame for plot_mass

--- pyIMD/visualization/figures.py
+++ pyIMD/visualization/figures.py
@@ -1,5 +1,5 @@
 import warnings
-from pandas import concat, DataFrame
+from pandas import concat
 from pyIMD.analysis.curve_fit import fit_function
 from plotnine import ggplot, aes, geom_line, geom_point, theme_bw
 
@@ -22,8 +22,9 @@ def plot_fitting(x, y, resonance_frequency, parameter):
     """
 
     y_fit = fit_function(x, resonance_frequency, parameter[0], parameter[1], parameter[2])
-    y_fit.name = 'Phase [rad]'
+    y_fit.name = 'Phase fit'
     x.name = 'Frequency [kHz]'
+    y.name = 'Phase [rad]'
     data = concat([x, y, y_fit], axis=1)
     col_names = list(data)
 
@@ -39,20 +40,20 @@ def plot_mass(calculated_cell_mass):
     """ Plots the resulting mass
 
     Args
-    :param calculated_cell_mass   List of cell mass
+    :param calculated_cell_mass   Dataframe [Nx2] with time and calculated cell mass data
 
     Return
     :return p:                    ggplot plot object
 
     """
 
-    x = list(range(0, len(calculated_cell_mass)))
-    data = concat([DataFrame(x, columns=['Time [iteration]']), DataFrame(calculated_cell_mass,
-                                                                         columns=['Mass [pg]'])], axis=1)
-    col_names = list(data)
+    col_names = list(calculated_cell_mass)
+    col_names[0] = 'Time [h]'
+    calculated_cell_mass.columns = col_names
 
     # Plot data
-    p = ggplot(aes(x=col_names[0], y=col_names[1]), data=data) + \
+    p = ggplot(aes(x=col_names[0], y=col_names[1]), data=calculated_cell_mass) + \
         geom_point(alpha=0.1) + \
+        geom_line(aes(y=col_names[2]), color='red') + \
         theme_bw()
     return p
