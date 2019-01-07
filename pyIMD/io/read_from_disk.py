5b5a285e822bddd2f0dcf18f3ac09a426832a82f
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2019-01-07 17:17:22 +0100

    Fixed docstring styling for documentation

--- pyIMD/io/read_from_disk.py
+++ pyIMD/io/read_from_disk.py
@@ -5,14 +5,13 @@ __author__ = 'Andreas P. Cuny'
 
 
 def read_from_tdms(file):
-    """read_from_tdms Reads data from tdms files and returns the data structured \
-       in a pandas data frame.
+    """Method to read data from National Instruments technical data management streaming files (TDMS).
 
     Args:
-    :param file:            File path + File name string.
+        file (`str`):              File path + File name string.
 
     Returns:
-    :return    df:          Returns data structured in a pandas data frame.
+        data (`pandas dataframe`):  Returns data structured in a pandas data frame.
 
     """
     tdms_file = TdmsFile(file)
@@ -21,18 +20,17 @@ def read_from_tdms(file):
 
 
 def read_from_text(file, delimiter, read_from_row):
-    """read_from_text Reads data from text files and returns the \
-       data structured in a pandas data frame.
+    """Method to read data from text files.
 
     Args:
-    :param file:              File path + File name string.
-    :param delimiter:         Delimiter used in the data file to seperate columns
-    :param read_from_row:     Row number from where to start reading data to be able \
-                              to skip heading text rows. Make sure that you keep the \
-                              Frequency, Amplitude and Phase headers.
+        file (`str`):               File path + File name.
+        delimiter (`str`):          Delimiter used in the data file to seperate columns
+        read_from_row (`int`):      Row number from where to start reading data to be able \
+                                    to skip heading text rows. Make sure that you keep the \
+                                    Frequency, Amplitude and Phase headers.
 
     Returns:
-    :return data:             Returns data structured in a pandas data frame.
+        data (`pandas dataframe`):  Returns data structured in a pandas data frame.
 
     """
     data = read_csv(file, sep=delimiter, skiprows=read_from_row)
