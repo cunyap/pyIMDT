b7aa47a5ac73f9bdc3eeb6acbc1d99c2f2273350
gflaesch <gotthold.flaeschner@bsse.ethz.ch>
2019-01-16 13:48:04 +0100

    Update curve_fit.py

--- pyIMD/analysis/curve_fit.py
+++ pyIMD/analysis/curve_fit.py
@@ -4,20 +4,21 @@ __author__ = 'Andreas P. Cuny'
 
 
 def fit_function(x, fn, q, a, b):
-    """fit_function Calculates the function fit
+    """fit_function is the phase response of a damped harmonic oscillator. It is called from calculate_resonance_frequencies, to be fitted to the data
+    primarily to extract the resonance frequency.
 
     Args:
-    :param x:              Frequency (the independent variable of that function)
+    :param x:              Frequency (the independent variable of the function)
     :param fn:             Resonance frequency
     :param q:              Q factor (losses)
     :param a:              Linear factor accounting for a linear background
     :param b:              Offset of the background
 
     Returns:
-    :return fit:           Returns fit as panda data frame
+    :return phase:           Returns the phase as panda data frame
     """
 
-    fit = -arctan(q * (fn * fn - x * x)/(fn * x)) + a * x + b
-    return fit
+    phase = -arctan(q * (fn * fn - x * x)/(fn * x)) + a * x + b
+    return phase
 
 
