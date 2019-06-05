bc0697022527cde97aa345b07b681d2f81425cd9
cunyap <acuny@bluewin.ch>
2019-06-05 13:57:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(name='pyIMD',
                    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                    'Topic :: Scientific/Engineering'
                    ],
-      install_requires=['pandas>=0.20.0', 'numpy>=1.14.5', 'scipy>=1.1.0', 'nptdms>=0.12.0', 'tqdm>=4.23.4',
+      install_requires=['pandas>=0.20.0', 'numpy>=1.14.5', 'scipy==1.1.0', 'nptdms>=0.12.0', 'tqdm>=4.23.4',
                         'plotnine>=0.3.0', 'PyQT5', 'lxml', 'xmltodict', 'matplotlib', 'pyyaml', 'pyqtgraph',
                         'xmlunittest'],
 
