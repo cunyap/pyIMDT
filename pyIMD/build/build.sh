c60241252b38bb0b9e7b5b54afb2a393faa5d72d
cunyap <acuny@bluewin.ch>
2019-06-07 21:13:03 +0200

    Update build.sh

--- pyIMD/build/build.sh
+++ pyIMD/build/build.sh
@@ -12,12 +12,12 @@ echo "# build the program"
 # see https://pythonhosted.org/PyInstaller/usage.html
 echo $1 == "linux"
 echo $1
-if [ $1 == "linux"]
+if [ $1 == "linux" ];
 then
 python3 -m PyInstaller --noconsole --onefile --icon=/home/travis/build/cunyap/pyIMDT/pyIMD/ui/icons/pyIMD_logo_icon.ico /home/travis/build/cunyap/pyIMDT/pyIMD/build/pyIMD_unix.spec
 else
 python3 -m PyInstaller --noconsole --onefile /home/travis/build/cunyap/pyIMDT/pyIMD/build/pyIMD_osx.spec
-fi
+fi;
 
 echo "# create the .exe file"
 # see http://stackoverflow.com/a/367826/1320237
